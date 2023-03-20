0134fcd4d5f333cba12dc84340551c9a411b53b5
Sebastián Guzmán <ivansebastiangf@gmail.com>
2023-03-19 19:51:32 -0500

    Initial commit

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {            
+            var client = new MongoClient(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+            var database = client.GetDatabase(Environment.GetEnvironmentVariable("DATABASE_NAME"));
+
+            Products = database.GetCollection<Product>(Environment.GetEnvironmentVariable("DATABASE_COLLECTION"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
